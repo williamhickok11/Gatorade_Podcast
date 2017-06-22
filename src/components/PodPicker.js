51bad4cb0963ccea2cd93005412a95da1cc592d7
William Hickok <whickok@gsandf.com>
2017-06-21 21:33:55 -0500

    ready to deply

--- src/components/PodPicker.js
+++ src/components/PodPicker.js
@@ -12,7 +12,7 @@ const PodCard = ({children}) => {
 const PodCardText = ({currentIndex, isActive, title, clickProp, i}) => {
   return (
       <div className={isActive === false ? "podcast-list-item pp-inactive" : "podcast-list-item"}>
-        <p className={currentIndex == i ? "selected" : ""} onClick={()=>clickProp(i)}>{title}</p>
+        <input type="button" className={currentIndex == i ? "selected" : ""} onClick={()=>clickProp(i)} value={title} />
       </div>
   );
 };
