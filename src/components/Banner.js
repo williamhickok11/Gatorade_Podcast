cd4fcfc40da478a6c084d91a1e1c0308836d29c1
William Hickok <whickok@gsandf.com>
2017-06-23 10:44:57 -0500

    Revisions to go live

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,12 +6,12 @@ const Banner = () => {
       <div className="innerBannerWrapper">
         <div className="page-title">
           <div className="banner-title">
-            <img className="main-title" src="./imgs/Gatorade-Main-Title.png" alt="Smiley face" />
-            <img className="main-title-mobile" src="./imgs/Title-Mobile.png" alt="Smiley face" />
+            <img className="main-title" src="../imgs/Gatorade-Main-Title.png" alt="Smiley face" />
+            <img className="main-title-mobile" src="../imgs/Title-Mobile.png" alt="Smiley face" />
           </div>
         </div>
         <div className="gatorade-logo">
-          <a href="http://www.gatorade.com/"><img src="./imgs/Logo_Gatorade.svg" alt="Smiley face"  /></a>
+          <a href="http://www.gatorade.com/"><img src="../imgs/Logo_Gatorade.svg" alt="Smiley face"  /></a>
         </div>
       </div>
     </div>
--- src/components/Details.js
+++ src/components/Details.js
@@ -31,9 +31,9 @@ class Details extends React.Component {
           <h1 className="intro-header">EACH EPISODE WILL START WITH THE SOUND FROM A GAME.</h1>
           <p>It’s one moment. It’s there and then it’s gone. In sports, losing takes a split second. Some losses are routine and some are transformative. This is a podcast series about the loss that changed everything for an athlete. Because there’s always a riveting story in the moments before and after that loss.</p>
           <div className="intro-image-div">
-            <a href="https://www.google.com"><img src="./imgs/Badge_Apple.svg" alt="" /></a>
-            <a href="https://www.google.com"><img src="./imgs/Spotify.png" alt="" /></a>
-            <a href="https://www.google.com"><img src="./imgs/Badge_Google_Play.svg" alt="" /></a>
+            <a href="https://www.google.com"><img src="../imgs/Badge_Apple.svg" alt="" /></a>
+            <a href="https://www.google.com"><img src="../imgs/Spotify.png" alt="" /></a>
+            <a href="https://www.google.com"><img src="../imgs/Badge_Google_Play.svg" alt="" /></a>
           </div>
         </div>
       }
