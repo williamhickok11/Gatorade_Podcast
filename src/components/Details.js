eaeced2142e13b4211fb591b5096dfe0999ed8ad
William Hickok <whickok@gsandf.com>
2017-06-24 12:47:46 -0500

    new iframe

--- src/components/Details.js
+++ src/components/Details.js
@@ -28,16 +28,16 @@ class Details extends React.Component {
       {
       this.props.currentView === "intro" &&
         <div className="intro-main">
-          <h1 className="intro-header">EACH EPISODE WILL START WITH THE SOUND FROM A GAME.</h1>
-          <p>The Secret to Victory is a branded podcast from Gatorade and Gimlet Creative about what happens when the best athletes in the world lose. We'll feature Serena Williams, Peyton Manning, Eli Manning, Matt Ryan, JJ Watt, Karl Anthony Towns,and Kyle Schwarber, and the people who know them best.</p>
-          <p>If you're new to podcasts, there are a bunch of places you can listen to The Secret to Victory. Stream it right here, or try out one of the links below:</p>
+          <h1 className="intro-header">Even the best athletes in the world lose. This is a podcast about how they face it.</h1>
+          <p>From Gatorade and Gimlet Creative, <i>The Secret to Victory</i> is about how the most competitive athletes in the world use defeat to fuel their next win. Featuring Serena Williams, Peyton Manning, Eli Manning, Matt Ryan, JJ Watt, Karl Anthony Towns, and Kyle Schwarber, and the people who know them best. Hosted by ESPN writer and former NFL player Domonique Foxworth.</p>
+          <p>If you’re new to podcasts, there are a bunch of places you can listen to <i>The Secret to Victory</i>. Stream it right here, or try out one of the links below:</p>
           <div className="intro-image-div">
             <a href="https://itunes.apple.com/us/podcast/id1252154385"><img src="../imgs/Badge_Apple.svg" alt="" /></a>
             <a href="https://www.google.com"><img src="../imgs/Spotify.png" alt="" /></a>
             <a href="https://play.google.com/music/listen#/ps/Iocylyx7uf5s6xaizk6ol2t276m"><img src="../imgs/Badge_Google_Play.svg" alt="" /></a>
           </div>
           <div className="rss-feed">
-            <h2>RSS for your podcatcher</h2>
+            <h2>Or, add this RSS feed to the player of your choice:</h2>
             <p>http://feeds.gimletcreative.com/thesecrettovictory</p>
           </div>
         </div>
@@ -47,11 +47,14 @@ class Details extends React.Component {
         <div className="about-main">
           <div className="about-text">
             <h1>Series Description:</h1>
-            <p>What happens when the best athletes in the world lose? From Gatorade and Gimlet Creative, a podcast about how the world's most competitive athletes use defeat as motivation to build strength from the inside out, and ultimately come out on top. Episodes featuring Serena Williams, Peyton Manning, Eli Manning, Matt Ryan, JJ Watt, Karl Anthony Towns,and Kyle Schwarber, and the people who know them best. Listen here, or subscribe wherever you get your podcasts.</p>
+            <p>What happens when the best athletes in the world lose? From Gatorade and Gimlet Creative, a podcast about how the world’s most competitive athletes use defeat as motivation to build strength from the inside out, and ultimately come out on top. Episodes featuring Serena Williams, Peyton Manning, Eli Manning, Matt Ryan, JJ Watt, Karl Anthony Towns,and Kyle Schwarber, and the people who know them best. Listen here, or subscribe wherever you get your podcasts.</p>
           </div>
           <div className="about-text">
             <h1>Host Bio: </h1>
             <p>Host Domonique Foxworth, a former cornerback in the National Football League, will bring his unique perspective as a professional athlete to each episode. Foxworth, who played seven seasons in the NFL, is the former president of the National Football League Players Association (NFLPA) and a journalist and commentator for ESPN.</p>
+          </div>
+          <div style={{width:'100%', margin:'20px auto'}}>
+            <iframe frameBorder="no" height="200" scrolling="no" src="https://player.megaphone.fm/GLT3091702141?light=true&auto=true" width="100%" />
           </div>
         </div>
       }
